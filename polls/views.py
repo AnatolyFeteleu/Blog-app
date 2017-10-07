dc4ee3c9d36c1c02216674ba7417a372d8c06986
anatolyfeteleu@gmail.com <feteleuxpfds3>
2017-10-07 18:59:13 +0700

    End chapter 5

--- polls/views.py
+++ polls/views.py
@@ -2,20 +2,31 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question, Choice
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    """
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    Контекст - это словарь, содержащий название переменных шаблона и соответствующие им значения.
+    """
 
-def details(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/details.html', {'question': question})
+    def get_queryset(self):
+        """return the last five questions"""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+
+class DetailsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
+
+
+class ResultsView (generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
